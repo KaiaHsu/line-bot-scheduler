688515ac8b5c3e748bf9e9a40532fdb5f9c38eb0
KaiaHsu <kaia.platinum@gmail.com>
2025-06-10 06:30:24 +0800

    LINE Bot 定時推播專案

--- scheduleManager.js
+++ scheduleManager.js
@@ -3,13 +3,19 @@ const nodeSchedule = require('node-schedule')
 
 const tasks = {}
 
+/**
+ * 新增推播排程
+ */
 function addTask({ groupId, groupName, date, time, imageUrl, text, client, adminUserIds = [] }) {
   const code = `${groupId}_${date}_${time}_${Date.now()}`
   const [hour, minute] = time.split(':')
   const [year, month, day] = date.split('-')
   const jobDate = new Date(year, month - 1, day, hour, minute)
 
-  tasks[code] = nodeSchedule.scheduleJob(jobDate, async function () {
+  // 保存 meta 資訊供查詢
+  const meta = { groupId, groupName, date, time, imageUrl, text }
+
+  const job = nodeSchedule.scheduleJob(jobDate, async function () {
     const messages = []
     if (imageUrl) messages.push({ type: 'image', originalContentUrl: imageUrl, previewImageUrl: imageUrl })
     if (text) messages.push({ type: 'text', text })
@@ -23,7 +29,6 @@ function addTask({ groupId, groupName, date, time, imageUrl, text, client, admin
         })
       }
     } catch (err) {
-      // 推播失敗時所有管理員都通知
       for (const adminId of adminUserIds) {
         await client.pushMessage(adminId, {
           type: 'text',
@@ -33,9 +38,15 @@ function addTask({ groupId, groupName, date, time, imageUrl, text, client, admin
     }
     delete tasks[code]
   })
+  // 讓 meta 可以查詢
+  job.meta = meta
+  tasks[code] = job
   return code
 }
 
+/**
+ * 刪除指定排程
+ */
 function deleteTask(code) {
   if (tasks[code]) {
     tasks[code].cancel()
@@ -45,4 +56,22 @@ function deleteTask(code) {
   return false
 }
 
-module.exports = { addTask, deleteTask }
+/**
+ * 查詢所有尚未執行的推播任務
+ * 回傳 [{ code, groupName, groupId, date, time, text }]
+ */
+function listTasks() {
+  return Object.entries(tasks).map(([code, job]) => {
+    const meta = job.meta || {}
+    return {
+      code,
+      groupName: meta.groupName,
+      groupId: meta.groupId,
+      date: meta.date,
+      time: meta.time,
+      text: meta.text
+    }
+  })
+}
+
+module.exports = { addTask, deleteTask, listTasks }
